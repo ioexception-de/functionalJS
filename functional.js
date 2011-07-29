88f6a321f4040ced4a13c4e3530cf134e1299546
Falco Nogatz <fnogatz@googlemail.com>
2011-07-29 18:12:23 +0200

    default functions defined

--- functional.js
+++ functional.js
@@ -1,5 +1,6 @@
 var Functional = function() {
 	var list = [];
+	var name = null;
 
 	/* HELPER functions */
 	function isArray(obj) {
@@ -41,6 +42,9 @@ var Functional = function() {
 				case "object":
 					list.push(arguments[i]);
 				break;
+				case "string": 
+					name = arguments[i];
+				break;
 				case "function":
 					var params = getParametersOfFunction(arguments[i]);
 					params.push(arguments[i]);
@@ -55,6 +59,9 @@ var Functional = function() {
 			switch(typeof arguments[i]) {
 				case "object":
 					list.unshift(arguments[i]);
+				break;
+				case "string": 
+					name = arguments[i];
 				break;
 				case "function":
 					var params = getParametersOfFunction(arguments[i]);
@@ -98,6 +105,11 @@ var Functional = function() {
 					return list;
 				};
 			break;
+			case "__name":
+				return function __name() {
+					return name;
+				};
+			break;
 			default:
 				return false;
 			break;
@@ -114,6 +126,12 @@ var Functional = function() {
 		function sameArguments(a, b) {
 			// a might be a hash, b must be an array
 			for (var i = 0; i < b.length; i++) {
+				//
+				if (isArray(a[i]) && a[i].length == 0 && b[i] == "[]")	// match "[]" as empty array
+					continue;
+				if (isArray(b[i]) && b[i].length == 0 && a[i] == "[]")	// match "[]" as empty array
+					continue;
+
 				if (b[i] !== a[i])
 					return false;
 			}
@@ -181,7 +199,13 @@ var Functional = function() {
 			for (var key in hsh) {
 				pattern = new RegExp("(\\W)"+key+"(?!\\w)", "g");
 				if (typeof hsh[key] === 'function') {
-					replacement = hsh[key].toString().replace(/^function (.+)\([^)]*\)\s*{(\s|.)*}$/, '$1');
+					var fctlName = hsh[key]('__name')
+					if (fctlName !== null) {
+						replacement = fctlName;
+					}
+					else {
+						replacement = hsh[key].toString().replace(/^function (.+)\([^)]*\)\s*{(\s|.)*}$/, '$1');
+					}
 				} else {
 					replacement = hsh[key];
 				}
@@ -237,7 +261,6 @@ var Functional = function() {
 				} else {
 					var bothN = getMin(arguments, list[i].slice(0, -1));
 					var matched = matchVariableArguments(arguments, list[i].slice(0, -1), bothN);
-
 					if (matched !== false) {
 						var ret = insertValuesIntoFunction(list[i].slice(-1)[0], matched, list[i].slice(0,-1));
 						var nargs = list[i].slice(bothN, -1);
@@ -272,55 +295,219 @@ var Functional = function() {
 
 var loadDefaults = function() {
 
-	/* ===== NUMBERS ===== */
-	add = new Functional(
-		function(a, b) { return a+b; }
+	/* ===== BOOLEANS ===== */
+	and = new Functional("and",
+		function(a, b) { return a && b; }
+	);
+	or = new Functional("or",
+		function(a, b) { return a || b; }
 	);
 
-	mult = new Functional(
+	/* ===== NUMBERS ===== */
+	lessthan = new Functional("lessthan", 
+		function(a, b) { return a < b; }
+	);
+	greaterthan = new Functional("greaterthan",
+		function(a, b) { return a > b; }
+	);
+	lessequal = new Functional("lessequal",
+		function(a, b) { return a <= b; }
+	);
+	greaterequal = new Functional("greaterequal",
+		function(a, b) { return a >= b; }
+	);
+	equal = new Functional("equal",
+		function(a, b) { return a === b; }
+	);
+	/* Defining "Re" functions the most times you need the particual lessthanRe(2) for
+	 * "x<2" instead of lessthan(2) which means "2<x" */
+	lessthanRe = new Functional("lessthanRe", function(a, b) { return b < a; });
+	greaterthanRe = new Functional("greaterthanRe", function(a, b) { return b > a; });
+	lessequalRe = new Functional("lessequalRe", function(a, b) { return b <= a; });
+	greaterequalRe = new Functional("greaterequalRe", function(a, b) { return b >= a; });
+	equalRe = equal;
+
+
+	abs = new Functional("abs",
+		function(a) { return (a < 0 ? -1*a : a); }
+	);
+	signum = new Functional("signum",
+		function(a) { return (a < 0 ? -1 : (a > 1 ? 1 : 0)); }
+	);
+	add = new Functional("add",
+		function(a, b) { return a+b; }
+	);
+	mult = new Functional("mult",
 		function(a, b) { return a*b; }
+	);
+	sub = new Functional("sub",
+		function(a, b) { return a-b; }
+	);
+	ratio = new Functional("ratio",
+		function(a, b) { return a/b; }
+	);
+	mod = new Functional("mod",
+		function(a, b) { return a % b; }
+	);
+	div = new Functional("div",
+		function(a, b) { return Math.floor(a / b); }
+	);
+	subRe = new Functional("subRe", function(a, b) { return b-a; });
+	ratioRe = new Functional("ratioRe", function(a, b) { return b/a; });
+	modRe = new Functional("modRe", function(a, b) { return b % a; });
+	divRe = new Functional("divRe", function(a, b) { return Math.floor(b / a); });
+
+
+	/* ===== STRINGS ===== */
+	unwords = new Functional("unwords",
+		["[a:b:rest]", function(a, b, rest) { return a+" "+unwords(unshift(b, rest)); }],
+		["[x:xs]", 		function(x, xs) { return x; }]
 	);
 
 
 	/* ===== LISTS ===== */
-	tail = new Functional(
-		["[x:xs]", function(x, xs) { return xs; }]
+	tail = new Functional("tail",
+		["[x:xs]", 		function(x, xs) { return xs; }]
 	);
-
-	head = new Functional(
-		["[x:_]", function(x) { return x; }]
+	head = new Functional("head",
+		["[x:_]", 		function(x) { return x; }]
 	);
 
 	/* It's not allowed to use non-functional methods within your function body.
 	 * The function body should only contain a return statement without any
 	 * additional variable or function usage.
-   * But at the moment there's no way to avoid the javascript's unshift function
+	 * But at the moment there's no way to avoid the javascript's unshift function
 	 * as there isn't any haskell-like "(x:xs)" operator to create a new list.
 	 */
-	unshift = new Functional(
-		["e", "list", function(e, list) { list.unshift(e); return list; }]
+	unshift = new Functional("unshift",
+		["e", 			"list", 	function(e, list) { list.unshift(e); return list; }]
 	);
-
-	last = new Functional(
+	push = new Functional("push",
+		["e", 			"list", 	function(e, list) { list.push(e); return list; }]
+	);
+	last = new Functional("last",
 		["[a:b:rest]", function(a, b, rest) { return last(unshift(b, rest)); }],
-		["[a:_]", function(a) { return a; }]
+		["[a:_]", 		function(a) { return a; }]
+	);
+	length = new Functional("length",
+		["[]", 			0],
+		["[x:xs]", 		function(x, xs) { return 1+length(xs); }]
 	);
 
-	length = new Functional(
-		["[x:xs]", function(x, xs) { return 1+length(xs); }],
-		["[]", function() { return 0; }]
+	foldr = new Functional("foldr",
+		["f", 			"i", 			"[]", 		function(f, i) { return i; }],
+		["f", 			"i", 			"[x:xs]", function(f, i, x, xs) { return f(x, foldr(f, i, xs)); }]
+	);
+	foldl = new Functional("foldl",
+		["f", 			"i", 			"[]", 		function(f, i) { return i; }],
+		["f",				"i", 			"[x:xs]",	function(f, i, x, xs) { return foldl(f, f(z, x), xs); }]
 	);
 
-	foldr = new Functional(
-		["f", "i", "[]", function(f, i) { return i; }],
-		["f", "i", "[x:xs]", function(f, i, x, xs) { return f(x, foldr(f, i, xs)); }]
+	sum = foldr(add, 0);
+	avg = new Functional("avg",
+		["[]",			0],
+		["l",				function(l) { return ratio(sum(l), length(l)); }]
 	);
 
-	//TODO:
-	//sum = foldr(add, 0);
+	map = new Functional("map",
+		["f", 			"[]", 		function(f) { return []; }],
+		["f", 			"[x:xs]", 	function(f, x, xs) { return unshift(f(x), map(f, xs)); }]
+	);
+	all = new Functional("all",
+		["f", 			"l", 			function(f, l) { return foldr(and, true, map(f, l)); }]
+	);
+	any = new Functional("any",
+		["f", 			"l", 			function(f, l) { return foldr(or, false, map(f, l)); }]
+	);
+	until = new Functional("until",
+		["cond", 		"f",			"i", 			function(cond, f, i) { return (cond(i) ? i : until(cond, f, f(i))); }]
+	); 
 
+	concat = new Functional("concat",
+		["l", 			"[]", 		function(l) { return l; }],
+		["[]", 			"l", 			function(l) { return l; }],
+		["l", 			"[x:xs]", function(l, x, xs) { return concat(push(x, l), xs); }]
+	);
+	take = new Functional("take",
+		[0, 				"l", 			function(l) { return []; }],
+		["n", 			"[]", 		function(n) { return []; }],
+		["n", 			"[x:xs]", function(n, x, xs) { return unshift(x, take(n-1, xs)); }]
+	);
+	takeWhile = new Functional("takeWhile",
+		["cond", 		"[]",			function(cond) { return []; }],
+		["cond", 		"[x:xs]",	function(cond, x, xs) { return (cond(x) ? unshift(x, takeWhile(cond, xs)) : []); }]
+	);
+	reverse = new Functional("reverse",
+		["[]",			[]],
+		["[x:xs]", 		function(x, xs) { return push(x, reverse(xs)); }]
+	);
+	drop = new Functional("drop", 
+		[0, 				"l", 			function(l) { return l; }],
+		["n",				"[]", 		function(n) { return []; }],
+		["n", 			"[x:xs]",	function(n, x, xs) { return drop(n-1, xs); }]
+	);
+	replicate = new Functional("replicate",
+		[0,				"e",			function(e) { return []; }],
+		["n",				"e",			function(n, e) { return unshift(e, replicate(n-1, e)); }]
+	);
 }
 
 
-exports.Functional = Functional;
-exports.loadDefaults = loadDefaults;
+getDefaults = function() {
+	loadDefaults();
+
+	return {
+		and: and,
+		or: or,
+		lessthan: lessthan,
+		lessthanRe: lessthanRe,
+		lessequal: lessequal,
+		lessequalRe: lessequalRe,
+		equal: equal,
+		equalRe: equalRe,
+		greaterequal: greaterequal,
+		greaterequalRe: greaterequalRe,
+		greaterthan: greaterthan,
+		greaterthanRe: greaterthanRe,
+		abs: abs,
+		signum: signum,
+		add: add,
+		sub: sub,
+		subRe: subRe,
+		mult: mult,
+		ratio: ratio,
+		ratioRe: ratioRe,
+		div: div,
+		divRe: divRe,
+		mod: mod,
+		modRe: modRe,
+		unwords: unwords,
+		tail: tail,
+		head: head,
+		unshift: unshift,
+		push: push,
+		last: last,
+		length: length,
+		foldr: foldr,
+		foldl: foldl,
+		sum: sum,
+		avg: avg,
+		map: map,
+		all: all,
+		any: any,
+		concat: concat,
+		until: until,
+		take: take,
+		takeWhile: takeWhile,
+		reverse: reverse,
+		drop: drop,
+		replicate: replicate
+	};
+};
+
+
+if (typeof exports !== 'undefined') {
+	exports.Functional = Functional;
+	exports.loadDefaults = loadDefaults;
+	exports.defaults = getDefaults;
+}
--- test.js
+++ test.js
@@ -1,20 +1,59 @@
-var sys = require('sys');
-
 var fctl = require('./functional');
 var Functional = fctl.Functional;
 fctl.loadDefaults();
 
 
+console.log("===== Test default functions =====");
+console.log("and(true,false): "								+ and(true,false));										// false
+console.log("or(true,false): "								+ or(true,false));										// true
+console.log("mult(-3,4): " 									+ mult(-3,4));												// -12
+console.log("length([1,2,3]): " 								+ length([1,2,3]));										// 3
+console.log("tail([1,2,3]): " 								+ tail([1,2,3]));											// [2,3]
+console.log("head([1,2,3]): " 								+ head([1,2,3]));											// 1
+console.log("reverse([1,2,3]): "								+ reverse([1,2,3]));										// [3,2,1]
+console.log("unshift(0, [1,2,3]): " 						+ unshift(0, [1,2,3]));									// [0,1,2,3]
+console.log("last([1,2,3]): " 								+ last([1,2,3]));											// 3
+console.log("concat([1,2],[3,4]): "							+ concat([1,2],[3,4]));									// [1,2,3,4]
+console.log("take(3, [1,2,3,4,5]): "						+ take(3, [1,2,3,4,5]));								// [1,2,3]
+console.log("replicate(3, 5): "								+ replicate(3, 5));										// [5,5,5]
+console.log("foldr(mult, 1, [1,2,3,4]): " 				+ foldr(mult, 1, [1,2,3,4]));							// 24
+console.log("sum([1,2,3,4]): " 								+ sum([1,2,3,4]));										// 10					| sum = foldr(add, 0)
+console.log("map([1,-2,3,-4]): "								+ map(abs, [1,-2,3,-4]));								// [1,2,3,4]
+console.log("all(lessthanRe(5), [1,2,3,4]): " 			+ all(lessthanRe(5), [1,2,3,4]));					// true				| use particular "Re" function as lessthan(3) means 3<x instead of x<3, 
+																																		//						  which you can create using lessthanRe(3)
+console.log("until(greaterthanRe(80), mult(2), 1): "	+ until(greaterthanRe(80), mult(2), 1)); 			// 128				| multiplicating with 2, beginning with 1, until it's greater than 80
 
-/* Alternativ functional implementation of the multiplication function */
-mult2 = new Functional(
-	[1, "n", function(n) { return n; }],
+
+console.log("\n===== Test particular functions =====");
+console.log("1) Particular usage of a self-defined avarage function which expects 3 parameters");
+avg2 = new Functional(
+	["a", "b", "c", function(a, b, c) { return (a+b+c)/3; }]
+);
+console.log("avg(1,3,3.5): " 									+ avg2(1,3,3.5));											// 2.5
+console.log("avg(4,6)(mult(2,4)): " 						+ avg2(4,6)(mult(2,4)));								// 6
+console.log("avg(5)(6,7): " 									+ avg2(5)(6,7));											// 6
+
+console.log("2) Particular usage of the default's mult within a the map function");
+console.log("map(mult(3), [1,2,3]): "						+ map(mult(3), [1,2,3]));								// [1,3,9]
+
+
+console.log("\n===== Test own functionals =====");
+console.log("1) Alternative functional implementation of the multiplication function");
+mult2 = new Functional("mult2",
 	["n", 1, function(n) { return n; }],
 	function(a, b) { if (a < 0) { return -1*mult2(-1*a, b); } else if (b < 0) { return -1*mult2(a,-1*b); } else { return a+mult2(a,b-1); } }
 );
-sys.puts("mult2(-3,4): " + mult2(-3,4));
+console.log("mult2(-3,4): " 									+ mult2(-3,4));											// -12
 
-/* Functional implementation for the faculty function - factorial, factorial2 and factorial3 are equivalent */
+console.log("2) Functional implementation of the recursive fibonacci function");
+fibonacci = new Functional("fibonacci",
+	[0, 0],
+	[1, 1],
+	function(n) { return fibonacci(n-1)+fibonacci(n-2); }
+);
+console.log("fibonacci(10): " 								+ fibonacci(10));											// 55
+
+console.log("3) Functional implementation for the faculty function - factorial, factorial2 and factorial3 are equivalent");
 factorial = new Functional (
 	[0, 1],
 	function(n) { return n * factorial(n-1); }
@@ -26,38 +65,13 @@ factorial2 = new Functional(
 factorial3 = new Functional(
 	function(n) { if (n > 1) { return n*factorial3(n-1); } else return 1; }
 );
-sys.puts("factorial2(6): " + factorial2(6));
+console.log("factorial2(6): " 								+ factorial2(6));											// 720
 
-/* Functional implementation of the recursive fibonacci function */
-fibonacci = new Functional(
-	[0, 0],
-	[1, 1],
-	function(n) { return fibonacci(n-1)+fibonacci(n-2); }
-);
-sys.puts("fibonacci(10): " + fibonacci(10));
 
-/* Functional implementation of the average function of three numbers to test the particular usage */
-avg = new Functional(
-	["a", "b", "c", function(a, b, c) { return (a+b+c)/3; }]
-);
-sys.puts("avg(1,3,3.5): " + avg(1,3,3.5));
-sys.puts("avg(4,6)(8): " + avg(4,6)(mult(2,4)));
-sys.puts("avg(5)(6,7): " + avg(5)(6,7));
-
-/* example of extending the knowledge base */
-sys.puts("mult(1,1) BEFORE extending the database: " + mult(1,1));
+console.log("\n===== Test extending the knowledge database =====");
+console.log("mult(1,1) before extending the database: " + mult(1,1));
 mult('__addOnTop', 
 	[1, 1, 42],
 	[3, 1, 3.1415926535]
 );
-sys.puts("mult(1,1) AFTER extended the database with __addOnTop([1,1,42]): " + mult(1,1));
-
-/* Test of the default functions */
-sys.puts("mult(-3,4): " + mult(-3,4));
-sys.puts("length([1,2,3]): " + length([1,2,3]));
-sys.puts("tail([1,2,3]): " + sys.inspect(tail([1,2,3])));
-sys.puts("head([1,2,3]): " + head([1,2,3]));
-sys.puts("unshift(0, [1,2,3]): " + sys.inspect(unshift(0, [1,2,3])));
-sys.puts("last([1,2,3]): " + last([1,2,3]));
-sys.puts("foldr(mult, 1, [1,2,3,4]): " + foldr(mult, 1, [1,2,3,4]));
-
+console.log("mult(1,1) after extended the database with __addOnTop([1,1,42]): " + mult(1,1));
